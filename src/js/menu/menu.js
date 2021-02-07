169d174eae2a34725f242022a758bc7beae2b9b5
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2021-02-07 16:17:56 +0200

    add header

---
+++ src/js/menu/menu.js
@@ -0,0 +1,19 @@
+const refs = {
+    menuButton: document.querySelector('.js-menu'),
+    closeButton: document.querySelector('.js-close'),
+    header: document.querySelector('.js-header'),
+    nav: document.querySelector('.js-nav'),
+}
+
+
+refs.header.addEventListener('click', evt => {
+    const { menuButton, closeButton, header, nav } = refs;
+    evt.preventDefault();
+    console.log(evt.target);
+    if (evt.target === menuButton) {
+        nav.classList.add('is-open')
+    }
+    if (evt.target === closeButton) {
+        nav.classList.remove('is-open')
+    }
+})

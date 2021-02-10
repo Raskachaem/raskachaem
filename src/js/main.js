d5a59c5ac5640d3cb2bfa857d02340563b2fa710
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2021-02-10 12:12:00 +0200

    добавил автопрокрутку

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,13 @@ import './popup/popup';
 import Splide from '@splidejs/splide';
 const splideContainer = document.querySelector('.splide');
 const sliderContainer = document.getElementById('slider');
-new Splide(splideContainer, { autoplay: true, pauseOnHover: true, interval: 5000 }).mount();
-new Splide(sliderContainer, { autoplay: true, pauseOnHover: true, interval: 5000 }).mount();
+new Splide(splideContainer, { rewind: true, autoplay: true, pauseOnHover: true, interval: 5000 }).mount();
+new Splide(sliderContainer, { rewind: true, autoplay: true, pauseOnHover: true, interval: 5000 }).mount();
 
 if (window.matchMedia('(min-width: 768px)').matches) {	
-	new Splide( sliderContainer, { autoplay: true, pauseOnHover: false, interval: 5000, gap: '10px', perPage: 2} ).mount();
+	new Splide( sliderContainer, { rewind: true, autoplay: true, pauseOnHover: false, interval: 5000, gap: '10px', perPage: 2} ).mount();
 }
 if (window.matchMedia('(min-width: 1280px)').matches) {	
-	new Splide( sliderContainer, { autoplay: false, pauseOnHover: false, interval: 5000, gap: '37px', perPage: 3} ).mount();
+	new Splide( sliderContainer, { rewind: true, autoplay: false, pauseOnHover: false, interval: 5000, gap: '37px', perPage: 3} ).mount();
 }
 
